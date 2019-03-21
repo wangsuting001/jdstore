8f415050fca4c3787d092932c5bf1d7179adc0e2
王素亭 <wangsuting@wangsutingdeMacBook-Pro.local>
2019-03-21 15:22:17 +0800

    Infrastructure for Shopping website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def admin_required
+    unless current_user.is_admin?
+      flash[:alert] = "不是管理员"
+      redirect_to "/"
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Jdstore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    CarrierWave.configure do |config|
+      config.asset_host = proc do |file|
+        "http://127.0.0.1:3000"
+      end
+    end
   end
 end
